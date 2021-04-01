cee5fca326f903f2c7b5999904579461caea85ba
Vikram <arshvikram@gmail.com>
2021-03-31 20:55:05 -0600

    Able to select and unselect photos

--- LISTR/AddListing.xaml.cs
+++ LISTR/AddListing.xaml.cs
@@ -20,9 +20,80 @@ namespace LISTR
     /// </summary>
     public partial class AddListing : Page
     {
+        String defaultpath = "";
+
         public AddListing()
         {
             InitializeComponent();
         }
+
+        private void SelectPhotos(object sender, MouseButtonEventArgs e)
+        {
+            if (defaultpath.Equals("")) {
+                defaultpath = img1.Source.ToString();
+            }
+
+            Microsoft.Win32.OpenFileDialog dialogue = new Microsoft.Win32.OpenFileDialog();
+
+            // set valid file extensions
+            dialogue.DefaultExt = ".png";
+            dialogue.Filter = "PNG Files (*.png)|*.png|JPEG Files (*.jpeg)|*.jpeg|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+            // display the open file dialogue
+            Nullable<bool> result = dialogue.ShowDialog();
+
+
+            if (result == true)
+            {
+                string filename = dialogue.FileName;;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(filename);
+                bitmap.EndInit();
+                if (img1.Source.ToString().Contains("default.jpg")) {
+                    img1.Source = bitmap;
+                    img1_label.Visibility = Visibility.Visible;
+                }
+                else if (img2.Source.ToString().Contains("default.jpg")) {
+                    img2.Source = bitmap;
+                    img2_label.Visibility = Visibility.Visible;
+                }
+                else if (img3.Source.ToString().Contains("default.jpg"))
+                {
+                    img3.Source = bitmap;
+                    img3_label.Visibility = Visibility.Visible;
+                }
+                else if (img4.Source.ToString().Contains("default.jpg"))
+                {
+                    img4.Source = bitmap;
+                    img4_label.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void img1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            img1.Source = new BitmapImage(new Uri(@defaultpath));
+            img1_label.Visibility = Visibility.Hidden;
+        }
+
+        private void img2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            img2.Source = new BitmapImage(new Uri(@defaultpath));
+            img2_label.Visibility = Visibility.Hidden;
+        }
+
+        private void img3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            img3.Source = new BitmapImage(new Uri(@defaultpath));
+            img3_label.Visibility = Visibility.Hidden;
+        }
+
+        private void img4_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            img4.Source = new BitmapImage(new Uri(@defaultpath));
+            img4_label.Visibility = Visibility.Hidden;
+        }
+
     }
 }
