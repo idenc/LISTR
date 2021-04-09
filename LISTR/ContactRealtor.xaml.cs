0740bd7b3959c67bee782170100c9415bf864f83
gurnoora21 <gurnooraujla10@gmail.com>
2021-04-08 19:46:16 -0600

    Tweaked Browsing Design, added Contact Realtor Functionality

---
+++ LISTR/ContactRealtor.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.UI.WebControls;
+using System.Windows.Controls.Primitives;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace LISTR
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class ContactRealtor : Popup
+    {
+        private readonly Browsing browsing;
+        private const string defaultText = "Hello";
+
+        public ContactRealtor(Browsing browsing)
+        {
+            InitializeComponent();
+            this.browsing = browsing;
+        }
+
+        private void SendClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Username.Text) || string.IsNullOrWhiteSpace(Password1.Password) || string.IsNullOrWhiteSpace(myTextBox.Text))
+            {
+                RegisterError.Text = "Please fill out at least your name, \r\n e-mail, and provide a message";
+                return;
+            }
+            else
+            {
+                this.IsOpen = false;
+            }
+
+      
+
+          
+
+            
+           
+        }
+
+        private void CancelClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.IsOpen = false;
+        }
+
+
+
+    }
+}
