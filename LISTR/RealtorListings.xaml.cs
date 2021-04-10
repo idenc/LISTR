727b40c50d986c84cfd4dd813f96fe3bf9799467
idenc <iden.craven@gmail.com>
2021-04-09 22:35:05 -0600

    Make past listings button work

--- LISTR/RealtorListings.xaml.cs
+++ LISTR/RealtorListings.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 using MongoDB.Driver;
 
 namespace LISTR
@@ -14,13 +15,15 @@ namespace LISTR
     /// </summary>
     public partial class RealtorListings : Page
     {
-        public static ObservableCollection<House> houses = new ObservableCollection<House>(MainWindow.houses.AsQueryable().ToList());
+        public static ObservableCollection<House> AllHouses = new ObservableCollection<House>(MainWindow.houses.AsQueryable().ToList());
+        private ObservableCollection<House> houses;
         private readonly MainWindow mainWindow;
 
         public RealtorListings()
         {
             mainWindow = (MainWindow)Application.Current.MainWindow;
-            Console.WriteLine("Num Houses: " + houses.Count);
+            Console.WriteLine("Num Houses: " + AllHouses.Count);
+            houses = new ObservableCollection<House>(AllHouses.Take(AllHouses.Count - 1));
             InitializeComponent();
 
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(MyControl.ItemsSource);
@@ -56,6 +59,41 @@ namespace LISTR
         private void ListingSearch(object sender, TextChangedEventArgs e)
         {
             CollectionViewSource.GetDefaultView(MyControl.ItemsSource).Refresh();
+        }
+
+        private void SoldClick(object sender, RoutedEventArgs e)
+        {
+            SoldButton.Background = Brushes.White;
+
+            Panel.SetZIndex(SoldButton, 10);
+            Panel.SetZIndex(ActiveButton, 0);
+            houses = new ObservableCollection<House>(AllHouses.Skip(AllHouses.Count - 1));
+            MyControl.ItemsSource = houses;
+
+            var bc = new BrushConverter();
+            ActiveButton.Background = bc.ConvertFrom("#FF9B9797") as Brush;
+        }
+
+        private void HouseEdit(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ActiveClick(object sender, RoutedEventArgs e)
+        {
+            ActiveButton.Background = Brushes.White;
+
+            Panel.SetZIndex(ActiveButton, 10);
+            Panel.SetZIndex(SoldButton, 0);
+            houses = new ObservableCollection<House>(AllHouses.Take(AllHouses.Count - 1));
+            MyControl.ItemsSource = houses;
+
+            var bc = new BrushConverter();
+            SoldButton.Background = bc.ConvertFrom("#FF9B9797") as Brush;
+        }
+
+        private void RealtorLoaded(object sender, RoutedEventArgs e)
+        {
+            MyControl.ItemsSource = houses;
         }
     }
 
