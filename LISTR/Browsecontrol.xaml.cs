7f4b01fd015645d703a4eb706303e0a7745d3704
idenc <iden.craven@gmail.com>
2021-04-13 18:18:56 -0600

    Added button animations on browsing

--- LISTR/Browsecontrol.xaml.cs
+++ LISTR/Browsecontrol.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
 namespace LISTR
 {
@@ -45,7 +48,7 @@ namespace LISTR
             {
                 myLISTR.favourites.Add(house);
             }
-            browsingPage.AdvanceHouse();
+            AnimationClick(sender as Button);
         }
 
         private void SkipClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -54,7 +57,7 @@ namespace LISTR
             {
                 myLISTR.skipped.Add(house);
             }
-            browsingPage.AdvanceHouse();
+            AnimationClick(sender as Button);
         }
 
         private void DislikeClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -63,8 +66,25 @@ namespace LISTR
             {
                 myLISTR.disliked.Add(house);
             }
-            browsingPage.AdvanceHouse();
+            AnimationClick(sender as Button);
         }
+
+        //private void PlayClickAnimation(Button srcButton) {
+        //    Button button = new Button();
+        //    button.Content = srcButton.Content;
+        //    button.Background = srcButton.Background;
+        //    button.Margin = new Thickness(500, 455, -331, -405);
+        //    MainGrid.Children.Add(button);
+
+        //    TranslateTransform trans = new TranslateTransform();
+        //    button.RenderTransform = trans;
+        //    DoubleAnimation anim1 = new DoubleAnimation(top, newY - top, TimeSpan.FromSeconds(10));
+        //    DoubleAnimation anim2 = new DoubleAnimation(left, newX - left, TimeSpan.FromSeconds(10));
+        //    trans.BeginAnimation(TranslateTransform.XProperty, anim1);
+        //    trans.BeginAnimation(TranslateTransform.YProperty, anim2)
+
+        //    DoubleAnimation animation = new DoubleAnimation(0, TimeSpan.FromSeconds(1));
+        //}
 
         private void BrowsecontrolLoaded(object sender, RoutedEventArgs e)
         {
@@ -81,6 +101,64 @@ namespace LISTR
             ScrollViewer scv = (ScrollViewer)sender;
             scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta / 10);
             e.Handled = true;
+        }
+
+        private void AnimationClick(Button srcButton)
+        {
+            Thickness margin;
+            Button animationButton;
+            if (srcButton.Name == "FavouriteButton")
+            {
+                margin = new Thickness(85, 455, -200, -405);
+                animationButton = FavouriteAnimationButton;
+            }
+            else if (srcButton.Name == "SkipButton")
+            {
+                margin = new Thickness(225, 455, -335, -405);
+                animationButton = SkippedAnimationButton;
+            }
+            else
+            {
+                margin = new Thickness(500, 455, -335, -405);
+                animationButton = DislikedAnimationButton;
+            }
+
+            Storyboard sb = FindResource("ClickAnimation") as Storyboard;
+            animationButton.Visibility = Visibility.Visible;
+
+            var toMargin = new Thickness
+            {
+                Left = margin.Left,
+                Right = margin.Right,
+                Top = margin.Top - 100,
+                Bottom = margin.Bottom
+            };
+            var ta = new ThicknessAnimation
+            {
+                BeginTime = new TimeSpan(0),
+                From = margin,
+                To = toMargin,
+                Duration = new Duration(TimeSpan.FromSeconds(0.55))
+            };
+            ta.SetValue(Storyboard.TargetNameProperty, "AnimationButton");
+            Storyboard.SetTargetProperty(ta, new PropertyPath(MarginProperty));
+
+            sb.Children.Add(ta);
+
+            Storyboard.SetTarget(sb, animationButton);
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += AnimationComplete;
+            timer.Interval = TimeSpan.FromSeconds(0.15);
+
+            sb.Begin();
+            timer.Start();
+        }
+
+        private void AnimationComplete(object sender, EventArgs e)
+        {
+            browsingPage.AdvanceHouse();
+            (sender as DispatcherTimer).Stop();
         }
     }
 }
